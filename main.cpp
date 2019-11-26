7c4157707474c67e05ccff2389b3869e20b88c4e
1915157 <chonsa0409@naver.com>
2019-11-26 15:25:12 +0900

    13-01

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+struct student {
+	int 	ID;
+	char 	name[10];
+	double	grade;
+};
+
+int main(void) {
+	struct student s1 = {24, "SongYeSuel", 4.3};
+	
+	
+	return 0;
+}
